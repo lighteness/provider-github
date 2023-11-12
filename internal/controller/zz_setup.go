16f212082a2572f0f1c714f6db109927419e1729
huangliang1203 <huangliang1203@gmail.com>
2023-11-12 20:16:32 +0800

    controller

--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -1,24 +1,26 @@
-/*
-Copyright 2021 Upbound Inc.
-*/
+// SPDX-FileCopyrightText: 2023 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
 
 package controller
 
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	"github.com/upbound/upjet/pkg/controller"
+	"github.com/crossplane/upjet/pkg/controller"
 
-	resource "github.com/upbound/upjet-provider-template/internal/controller/null/resource"
-	providerconfig "github.com/upbound/upjet-provider-template/internal/controller/providerconfig"
+	branch "github.com/upbound/provider-github/internal/controller/branch/branch"
+	providerconfig "github.com/upbound/provider-github/internal/controller/providerconfig"
+	repository "github.com/upbound/provider-github/internal/controller/repository/repository"
 )
 
 // Setup creates all controllers with the supplied logger and adds them to
 // the supplied manager.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
-		resource.Setup,
+		branch.Setup,
 		providerconfig.Setup,
+		repository.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err

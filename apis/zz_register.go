26447a262f7b9766890a2a7314ff909baccc5b05
huangliang1203 <huangliang1203@gmail.com>
2023-11-12 20:15:18 +0800

    apis

--- apis/zz_register.go
+++ apis/zz_register.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 /*
 Copyright 2022 Upbound Inc.
 */
@@ -10,15 +14,17 @@ package apis
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
-	v1alpha1 "github.com/upbound/upjet-provider-template/apis/null/v1alpha1"
-	v1alpha1apis "github.com/upbound/upjet-provider-template/apis/v1alpha1"
-	v1beta1 "github.com/upbound/upjet-provider-template/apis/v1beta1"
+	v1alpha1 "github.com/upbound/provider-github/apis/branch/v1alpha1"
+	v1alpha1repository "github.com/upbound/provider-github/apis/repository/v1alpha1"
+	v1alpha1apis "github.com/upbound/provider-github/apis/v1alpha1"
+	v1beta1 "github.com/upbound/provider-github/apis/v1beta1"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes,
 		v1alpha1.SchemeBuilder.AddToScheme,
+		v1alpha1repository.SchemeBuilder.AddToScheme,
 		v1alpha1apis.SchemeBuilder.AddToScheme,
 		v1beta1.SchemeBuilder.AddToScheme,
 	)

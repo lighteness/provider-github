98225c0c6549dcac9bc8c98f494817c8aca4472e
huangliang1203 <huangliang1203@gmail.com>
2023-11-12 20:17:57 +0800

    external names

--- config/external_name.go
+++ config/external_name.go
@@ -4,13 +4,18 @@ Copyright 2022 Upbound Inc.
 
 package config
 
-import "github.com/upbound/upjet/pkg/config"
+import "github.com/crossplane/upjet/pkg/config"
 
 // ExternalNameConfigs contains all external name configurations for this
 // provider.
 var ExternalNameConfigs = map[string]config.ExternalName{
 	// Import requires using a randomly generated ID from provider: nl-2e21sda
 	"null_resource": config.IdentifierFromProvider,
+	// Name is a parameter and it is also used to import the resource.
+	"github_repository": config.NameAsIdentifier,
+	// The import ID consists of several parameters. We'll use branch name as
+	// the external name.
+	"github_branch": config.TemplatedStringAsIdentifier("branch", "{{ .parameters.repository }}:{{ .external_name }}:{{ .parameters.source_branch }}"),
 }
 
 // ExternalNameConfigurations applies all external name configs listed in the
